05595f886a898268ccefd8dff490d11017fbfad6
shoaibshaikhcl <shoaib.shaikh@clairvoyantsoft.com>
2020-07-05 12:34:39 +0530

    created empty batch process project

---
+++ src/test/java/com/shoaibshaikh/csv/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoaibshaikh.csv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
